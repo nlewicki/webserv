68e948af36c3029ca3f27e981855873d41ecbbc9
¨mhummel7¨ <mhummel@student.42heilbronn.de>
2025-10-29 10:49:42 +0100

    config - data construct

---
+++ cgi-bin/hello.php
@@ -0,0 +1,5 @@
+<?php
+echo "Content-Type: text/html\n\n";
+echo "<h1>Hello from PHP CGI!</h1>";
+echo "<p>Es funktioniert!</p>";
+?>
---
+++ cgi-bin/hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+print("Content-Type: text/html\n")
+print("<h1>Hello from Python CGI!</h1>")
+print("<p>Es funktioniert!</p>")
